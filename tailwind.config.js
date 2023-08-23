233574e67298c23735d0165545f4f37778b165c1
ilhamwahid <ilham.wahid06@gmail.com>
2023-08-23 09:38:06 +0700

    fect: setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+      },
+      colors: {
+        primaryColor: "hsl(359, 100%, 35%)",
+        primaryColorAlt: "hsl(359, 100%, 25%)",
+        titleColor: "hsl(19, 16%, 15%)",
+        textColor: "hsl(19, 16%, 35%)",
+        textColorLight: "hsl(19, 8%, 55%)",
+        bodyColor: "hsl(36, 52%, 87%)",
+        containerColor: "hsl(19, 100%, 97%)",
+      },
+    },
+    fontFamily: {
+      bodyFont: ["Poppins", "sans-serif"],
+      titleFont: ["Titan One", "cursive"],
+      subtitleFont: ["Dancing Script", "cursive"],
+    },
+    container: {
+      center: true,
+      padding: {
+        default: "1.5rem",
+        sm: "1rem",
+      },
+    },
   },
+
   plugins: [],
 };
